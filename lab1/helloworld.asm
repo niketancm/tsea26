611884c993addbe38440dae37a7e1dca177b505c
niketancm <niketancm@gmail.com>
2014-09-13 16:16:35 +0200

    testing

--- lab1/helloworld.asm
+++ lab1/helloworld.asm
@@ -9,12 +9,12 @@
 	set   r0,0		; Loop counter
 	set   r1,43		; Loop end
 
-loop
-	out   0x11,r0		; Output data
-	add   r0,1
-	cmp   r1,r0		; And check if we have reached the end of the loop
-	jump.ne loop
-
+;; loop
+;; 	out   0x11,r0		; Output data
+;; 	add   r0,1
+;; 	cmp   r1,r0		; And check if we have reached the end of the loop
+;; 	jump.ne loop
+ 
 ;;;     The following commented code is an example of a faster way of running the same loop
 	
 ;;; 	set   r0,0
