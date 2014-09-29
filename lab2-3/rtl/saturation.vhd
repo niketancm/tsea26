fdab2a872ca562ff653e331d2e04ef964ac291a2
niketancm <niketancm@gmail.com>
2014-09-29 19:28:11 +0200

    bug fix

--- lab2-3/rtl/saturation.vhd
+++ lab2-3/rtl/saturation.vhd
@@ -13,7 +13,9 @@ end saturation;
 
 architecture saturation_rtl of saturation is
 begin  -- saturation_rtl
-
+if(value_i(39 downto 32) /= 8b'00000000)
+  
+end if
   -- Remove the following lines and put your code here
   value_o <= value_i;
   did_sat_o <= '0';
